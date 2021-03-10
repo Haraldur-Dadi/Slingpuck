afa72ac65238b44b0d29289608465731d8d96678
Haraldur-Dadi <Halli.heima@gmail.com>
2021-03-10 21:05:50 +0000

    Make gap in the middle move

---
+++ Assets/MovingBarrier.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovingBarrier : MonoBehaviour {
+    private Vector3 target;
+    public float speed = 0.5f;
+
+    private void Start() {
+        target = new Vector3(0.75f, 0f, 0f);
+    }
+
+    private void LateUpdate() {
+        if (GameManager.Instance.playing) {
+            // Move side to side
+            transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, target) < 0.001f) {
+                // Swap the position of the cylinder.
+                target.x *= -1f;
+            }
+        }
+    }
+}
